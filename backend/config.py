3c23e88135476edabde93ded3deaf3a3bb23758b
Frank Lee <cc.frankee@gmail.com>
2025-03-29 23:14:07 -0700

    socket connection
    -adds an example webhook for Bland (doesn't do anything currently)
    -adds a socket connection to the Dashboards saved properties listing
    -adds a CallStarter for Bland (needs to be moved from frontend to backend)
    -removes node_modules

--- backend/config.py
+++ backend/config.py
@@ -8,3 +8,4 @@ class Config:
     DEEPGRAM_API_KEY = os.getenv("DEEPGRAM_API_KEY")
     ELEVENLABS_API_KEY = os.getenv("ELEVENLABS_API_KEY")
     ELEVENLABS_VOICE_ID= os.getenv("ELEVENLABS_VOICE_ID")
+    BLAND_API_KEY = os.getenv("BLAND_API_KEY")
